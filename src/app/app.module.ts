5811dec12c6ea5e7e31889f0290a734f887d93fb
Khalid Dahir <khaldlee@hotmail.com>
2019-10-17 09:30:59 +0200

    Added all components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,62 @@
 import { BrowserModule } from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import { NgModule } from '@angular/core';
 
+// Components
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { ContactComponent } from './contact/contact.component';
+import { TicketsComponent } from './tickets/tickets.component';
+import { RepairsComponent } from './repairs/repairs.component';
+import { TicketDetailsComponent } from './ticket-details/ticket-details.component';
+import { HomeComponent } from './home/home.component';
+import { NotfoundComponent } from './notfound/notfound.component';
+import { MenuComponent } from './menu/menu.component';
+
+// Routing
+import {RouterModule, Routes} from '@angular/router';
+import { LayoutModule } from '@angular/cdk/layout';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatIconModule } from '@angular/material/icon';
+import { MatListModule } from '@angular/material/list';
+
+
+const appRoutes: Routes = [
+  {path: '', component: HomeComponent},
+  {path: 'menu', component: MenuComponent},
+  {path: 'contact', component: ContactComponent},
+  {path: 'repairs', component: RepairsComponent},
+  {path: 'tickets', component: TicketsComponent},
+  {path: 'tickets/ticketId', component: TicketDetailsComponent},
+  {path: '**', component: NotfoundComponent}
+];
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    ContactComponent,
+    TicketsComponent,
+    RepairsComponent,
+    TicketDetailsComponent,
+    HomeComponent,
+    NotfoundComponent,
+    MenuComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    BrowserAnimationsModule,
+    AppRoutingModule,
+    RouterModule,
+    RouterModule.forRoot(
+      appRoutes),
+    LayoutModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatSidenavModule,
+    MatIconModule,
+    MatListModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/repairs/repairs.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-repairs',
+  templateUrl: './repairs.component.html',
+  styleUrls: ['./repairs.component.scss']
+})
+export class RepairsComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
